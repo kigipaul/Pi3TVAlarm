af30fd4033a5009da0921acde6fc8d4f2d3fc13c
kigipaul <kigipaul@kigi-VPS.(none)>
2016-03-07 01:26:05 +0800

    Add btn2stop first version

---
+++ btn2stop.py
@@ -0,0 +1,17 @@
+import time
+import os
+import RPi.GPIO as GPIO
+
+PIN = 18
+COUNT = 0
+END = 6
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PIN, GPIO.IN)
+
+while True:
+  if COUNT == END:
+    COUNT = 0
+	os.system("killall Pi3TVClock.py")
+  if GPIO.input(PIN):
+    count += 1
+  time.sleep(0.5)
